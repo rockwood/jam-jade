88593100fb9b0ef2fb2cf7a67951b72bca987a3f
Kevin Rockwood <kevin@rockwood.me>
2012-07-30 16:36:49 -0400

    initial commit

---
+++ jade.js
@@ -0,0 +1,18 @@
+define(function(require) {
+
+    var Jade = require('jade');
+
+    return {
+        load: function(name, require, callback, config) {
+            config = config || (config = {});
+            config.jade = config.jade || (config.jade = {});
+            config.jade.extension = config.jade.extension || (config.jade.extension = 'jade');
+
+            require(['text!' + name + '.' + config.jade.extension], function(content) {
+                var template = Jade.compile(content);
+                callback(template);
+            });
+        }
+    };
+
+});
